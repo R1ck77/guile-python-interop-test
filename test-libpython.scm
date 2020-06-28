24abf303328c63466365e3462b641a459fd86367
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-06-28 15:37:04 +0200

    Added double conversion test

--- test-libpython.scm
+++ test-libpython.scm
@@ -14,6 +14,16 @@
       (if (not (= value result))
           (error (format #f "Value not converted correctly: ~d expected, got ~d!" value result))))))
 
+;;; TODO/FIXME duplication
+(define (double-conversion-test value)
+  (format #t "About to convert ~f to Python…\n" value)  
+  (let ((py-float (pyfloat-from-double value)))
+    (format #t "Value converted to ~a! Getting the result back…\n" py-float)
+    (let ((result (pyfloat-as-double py-float)))
+      (format #t "Got ~f back\n" result)
+      (if (not (= value result))
+          (error (format #f "Value not converted correctly: ~f expected, got ~f!" value result))))))
+
 (define (finalization-test repetitions)
   (do ((i 0 (1+ i)))
       ((> i repetitions))
@@ -25,7 +35,8 @@
 
 (conversion-test 42)
 (finalization-test 10)
+(double-conversion-test 3.1415)
 
 ;;; Finalize Python
 (py-finalize)
-(display "Invocation successful!\n")
+(display "Test successful!\n")
