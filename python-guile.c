4232073946ca936fd42245dda76b7b46458b657c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-06-28 12:16:36 +0200

    Add integer conversion

--- python-guile.c
+++ python-guile.c
@@ -3,19 +3,61 @@
 #include <libguile.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
-static SCM initialize_python_wrapper(SCM unused) {
+static SCM PyObject_type;
+
+struct PyObject_data {
+  PyObject *object;
+  SCM name;
+  SCM update_func;
+};
+  
+
+static SCM Py_Initialize_wrapper(SCM unused) {
   Py_Initialize();
   return SCM_UNSPECIFIED;
 }
 
-static SCM finalize_python_wrapper(SCM unused) {
+static SCM Py_Finalize_wrapper(SCM unused) {
   Py_Finalize();
   return SCM_UNSPECIFIED;
 }
 
+static SCM PyLong_FromLongLong_wrapper(SCM value) {
+  if(!scm_integer_p(value)) {
+    // TODO/FIXME extract, generalize
+    scm_error_scm (scm_from_locale_symbol ("value error"),
+                   scm_from_utf8_string ("PyLong_FromLongLong"),
+                   scm_from_utf8_string ("Invalid value provided"),
+                   SCM_UNSPECIFIED,
+                   scm_list_1 (scm_from_int (1)));
+  } else {
+    long long int_value = scm_to_signed_integer(value, LONG_MIN, LONG_MAX);
+    PyObject *py_value = PyLong_FromLongLong(int_value);
+
+    struct PyObject_data *pyobject_data  = (struct PyObject_data *) scm_gc_malloc (sizeof (struct PyObject_data), "PyLong");
+    pyobject_data->object = py_value;
+    
+    // TODO/FIXME Python garbage collection completely missing! I need probably some kind of thread guard
+    return scm_make_foreign_object_1(PyObject_type, pyobject_data);
+  }
+}
+
+static SCM PyLong_AsLongLong_wrapper(SCM value) {
+  scm_assert_foreign_object_type(PyObject_type, value);
+  struct PyObject_data *pyobject_data = scm_foreign_object_ref(value, 0);
+  long long int_value = PyLong_AsLongLong(pyobject_data->object);
+  return scm_from_signed_integer(int_value);
+}
+
 void init_python()
 {
-  scm_c_define_gsubr("py-initialize", 0, 0, 0, initialize_python_wrapper);
-  scm_c_define_gsubr("py-finalize", 0, 0, 0, initialize_python_wrapper);
+  PyObject_type = scm_make_foreign_object_type(scm_from_utf8_symbol("PyObject"),
+                                               scm_list_1(scm_from_utf8_symbol("pointer")),
+                                               NULL);
+  scm_c_define_gsubr("py-initialize", 0, 0, 0, Py_Initialize_wrapper);
+  scm_c_define_gsubr("pylong-from-long", 1, 0, 0, PyLong_FromLongLong_wrapper);
+  scm_c_define_gsubr("pylong-as-long", 1, 0, 0, PyLong_AsLongLong_wrapper);
+  scm_c_define_gsubr("py-finalize", 0, 0, 0, Py_Finalize_wrapper);
 }
--- test-libpython.scm
+++ test-libpython.scm
@@ -1,8 +1,24 @@
+(use-modules (ice-9 format))
 
+;;; Load the module
 (define python-lib (dynamic-link "libpython-guile.so"))
 (dynamic-call "init_python" python-lib)
 
+;;; Initialize Python
 (display "Calling Python procedures\n")
 (py-initialize)
+
+(define (conversion-test value)
+  (format #t "About to convert ~d to Python…\n" value)  
+  (let ((py-long (pylong-from-long value)))
+    (format #t "Value converted to ~a! Getting the result back…\n" py-long)
+    (let ((result (pylong-as-long py-long)))
+      (format #t "Got ~d back\n" result)
+      (if (not (= value result))
+          (error (format #f "Value not converted correctly: ~d expected, got ~d!" value result))))))
+
+(conversion-test 42)
+
+;;; Finalize Python
 (py-finalize)
 (display "Invocation successful!\n")
