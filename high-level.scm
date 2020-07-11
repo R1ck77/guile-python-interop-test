9b8140abd7d12f65656a2c31d519ac319ad34a4a
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-07-11 13:52:01 +0200

    Add high-level example (script execution)

--- high-level.scm
+++ high-level.scm
@@ -1,22 +1,39 @@
 (define-module (high-level))
 (use-modules (python))
 
-(define (incref py-object)
+(define-macro (with-python . code)
+  (let ((exception (make-symbol "exception")))
+    `(begin
+       (use-modules (srfi srfi-18))
+       (py-initialize)
+       (with-exception-handler
+        (lambda (,exception)
+          (py-finalize)
+          (raise ,exception))
+        (lambda ()
+          ,@code
+          (py-finalize))))))
+
+
+(define (incref! py-object)
   (py-incref py-object)
   py-object)
 
+
 (define (get-builtins-copy)
   (pydict-copy
-   (incref
-    (pyeval-get-builtins))))
+   (incref! (pymodule-get-dict
+     (incref! (pyimport-add-module "__main__"))))))
 
 (define (run-code code globals locals)
-  12
-  
-  )
+  (let ((compiled-code (py-compile-string code "<guile>" py-file-input)))
+    (list (cons 'result (and compiled-code
+                             (pyeval-eval-code compiled-code globals locals)))
+          (cons 'locals locals)
+          (cons 'globals globals))))
 
 
+(export with-python)
 (export run-code)
 (export get-builtins-copy)
-
-
+(export incref!)
--- python-demo.scm
+++ python-demo.scm
@@ -1,11 +1,21 @@
 (define-module (python-demo))
+(use-modules (ice-9 format))
 (use-modules (high-level))
 (use-modules (python))
 
-(py-initialize)
+(with-python
+ (let ((locals (get-builtins-copy)))
+   (pydict-set-item-string locals "a" (pyfloat-from-double 4))
+   (pydict-set-item-string locals "b" (pyfloat-from-double 3))   
+   (let ((result (run-code "import math
 
-(define locals (get-builtins-copy))
-(run-code "print(\"Hello, World!\")" locals locals)
+def f(x, y):
+    return math.sqrt(math.pow(x, 2) + math.pow(y, 2))
+
+c = f(a, b)
+" locals locals)))
+     (format #t "The result of the script execution is: ~g\n" (pyfloat-as-double
+                                                               (incref!
+                                                                (pydict-get-item-string (assq-ref result 'locals) "c")))))))
 
 
-(py-finalize)
